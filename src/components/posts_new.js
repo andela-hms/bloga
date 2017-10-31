dffa926cc03100f2cfc9e2d362760d550fb73175
andela-hms <humphrey.musonye@andela.com>
2017-10-31 21:22:57 +0300

    (feat): Create posts new form with redux form - integrate redux form - add validation to the fields - render jsx for field components

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,70 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{ field.label }</label>
+                <input
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div>
-                Posts New
-            </div>
+            <form>
+                <Field 
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+
+                <Field
+                    label="Categories"                
+                    name="categories"
+                    component={this.renderField}
+                />
+
+                <Field 
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+function validate(values) {
+    const errors = {};
+
+    // validate inputs from values object
+    if ( !values.title ) {
+        errors.title = "Enter a title";
+    }
+
+    if ( values.title.length <= 3 ) {
+        errors.title = "Title should be atleast 3 xcters long";
+    }
+
+    if ( !values.categories ) {
+        errors.title = "Enter some categories please";
+    }
+
+    if ( !values.content ) {
+        errors.title = "Enter some content please";
+    }
+
+    // if errors empty, the form is fine to submit
+    // if errors has any properties, redux form assumes the form invalid 
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+}) (PostsNew);
